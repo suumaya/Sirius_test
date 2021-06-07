0021cd26dcec48845ddf35f857f5404032979123
Sumaya Altamimi <smsomi9@hotmail.com>
2021-06-07 15:09:30 +0300

    obj

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('./logger');
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
 
 //console.log(logger);
 //logger.log("this is my msg");
@@ -18,9 +20,9 @@ var totalmem = os.totalmem();
 //const files = fs.readdirSync('./');
 //console.log(files);
 
-fs.readdir('./',function(err,files){
-if (err) console.log('Error: ', err);
-else console.log('Result: ',files);
-});
+// fs.readdir('./',function(err,files){
+// if (err) console.log('Error: ', err);
+// else console.log('Result: ',files);
+// });
 
 
