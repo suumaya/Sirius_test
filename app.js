5744dd68958626713ad75e2d996f80591a4b63e3
Sumaya Altamimi <smsomi9@hotmail.com>
2021-06-07 06:04:51 +0300

    some mods

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
-var logger = require('./logger');
-console.log(logger);
+const logger = require('./logger');
+const path = require("path");
+const os = require("os");
 
-logger.log("this is my msg");
+//console.log(logger);
+//logger.log("this is my msg");
+
+var pathObj = path.parse(__filename);
+//console.log(pathObj);
+
+var freemem = os.freemem();
+var totalmem = os.totalmem();
+//console.log('Free memory: ' + freemem);
+//console.log(`Total memory:  ${totalmem}`);
+
