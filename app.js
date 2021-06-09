7ff80f1bcaa00e8c6a26e77bdb1ce2d28eadca91
Sumaya Altamimi <smsomi9@hotmail.com>
2021-06-09 11:47:40 +0300

    http

--- app.js
+++ app.js
@@ -5,6 +5,41 @@ const os = require("os");
 const fs = require("fs");
 const EventEmitter = require("events");
 // const emitter = new EventEmitter(); // will use logger
+
+//*Http first way *//
+
+// const http = require('http');
+// this server is also event emitter
+// const server = http.createServer(); 
+// On : to handle an event (request)
+// register a listener: the event name is connection as in docs:
+// server.on("connection",(socket) => {
+//     console.log("new connection.....");
+// });
+// Listen : every new connection/request will raise an event
+// server.listen(80);
+// console.log("Listening on port 80...");
+
+//* Http 1st done *//
+
+//* Http second way ( better ) *//
+const http = require('http');
+const server = http.createServer((req,res)=>{
+if (req.url == "/"){
+    res.write("Root Page!");
+    res.end();
+}
+if (req.url == "/api/courses"){
+    res.write(JSON.stringify([1,2,3]));
+    res.end();
+}
+}); 
+server.listen(80);
+console.log("Listening on port 80...");
+
+//* Http done *//
+
+
 logger = new Logger();
 
 // Register a listener
@@ -13,7 +48,7 @@ logger.on('messageLogged',(arg) => { //same as function(arg){}
 });
 
 // console.log(logger);
-logger.log("this is my msg");
+// logger.log("this is my msg");
 
 var pathObj = path.parse(__filename);
 //console.log(pathObj);
